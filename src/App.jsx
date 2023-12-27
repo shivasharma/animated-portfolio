7ab164c7b2aaba0615f2f25f8b7b877a7aac8f87
Shiva Sharma <shivathebravo@gmail.com>
2023-12-27 12:46:11 -0500

    updated website

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Cursor />
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
+    </div>
+  );
 };
 
 export default App;
